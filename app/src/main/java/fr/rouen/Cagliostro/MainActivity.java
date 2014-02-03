5be24b94a6b10a8fd6fa992396bf3044e31f7984
Jean-André Santoni <jean.andre.santoni@gmail.com>
2014-02-03 17:15:49 +0100

    Navigation

--- app/src/main/java/fr/rouen/Cagliostro/MainActivity.java
+++ app/src/main/java/fr/rouen/Cagliostro/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.os.Debug;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -13,8 +12,7 @@ import ir.noghteh.JustifiedTextView;
 import android.util.TypedValue;
 import android.graphics.Paint.Align;
 import android.widget.TextView;
-
-import java.io.Console;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
 
@@ -24,18 +22,18 @@ public class MainActivity extends Activity {
 
         float d = getResources().getDisplayMetrics().density;
 
-        String[] episode1Array = getResources().getStringArray(R.array.episode1);
+        String[] episode1Array = getResources().getStringArray(R.array.ep1_paragraphs);
 
         setContentView(R.layout.episode);
 
         TableLayout table = (TableLayout)findViewById(R.id.tableLayout);
 
         TextView title = (TextView)findViewById(R.id.title);
-        title.setText(getResources().getString(R.string.title1));
+        title.setText("1. " + getResources().getString(R.string.ep1_title));
         title.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/SuperClarendon_7.otf"));
 
         TextView subtitle = (TextView)findViewById(R.id.subtitle);
-        subtitle.setText(getResources().getString(R.string.subtitle1));
+        subtitle.setText(getResources().getString(R.string.ep1_subtitle));
         subtitle.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/SuperClarendon_7.otf"));
 
         for (String s : episode1Array) {
@@ -63,14 +61,13 @@ public class MainActivity extends Activity {
         }
 
         Button next = (Button)findViewById(R.id.nextButton);
-        next.setText("Episode 2.\nDes heures s'écoulèrent");
+        next.setText("Episode 2\n" + getResources().getString(R.string.ep1_nexttitle));
         next.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/SuperClarendon_7.otf"));
         next.setLineSpacing(0, 1.3f);
-        next.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                System.out.println("BOUTON");
-            }
-        });
     }
 
+    public void nextEpisode(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
